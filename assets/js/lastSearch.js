ab8280cfe1066a4c6e2fadfd8c150b5af3349c19
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-02-03 16:00:53 -0800

    Created current weather js file

--- assets/js/lastSearch.js
+++ assets/js/lastSearch.js
@@ -1,14 +1,14 @@
-var getButtonValue = function (value) {
+var buttonVal = function (value) {
     var buttonValue = value.innerHTML;
     locationCall(buttonValue);
 };
 
 //function to create a button. Called multiple times in retrieveLocalStorage based on local storage length
 var buttonCreation = function (content) {
-    var citybtn = document.createElement('button');
-    citybtn.textContent = content;
+    var currentCityBtn = document.createElement('button');
+    currentCityBtn.textContent = content;
     //onclick, this function will run that calls another function that grabs the value of the button
-    citybtn.onclick = function () { getButtonValue(this); };
+    currentCityBtn.onclick = function () { buttonVal(this); };
     //returns the button
-    return citybtn
+    return currentCityBtn
 };
